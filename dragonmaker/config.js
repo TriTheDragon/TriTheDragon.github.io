f2c26b2529e46048ea9d66818eff50177dda7067
Tom Mudway <tmudway@hotmail.co.uk>
2024-03-12 17:37:05 -0400

    Populated config file

---
+++ dragonmaker/config.js
@@ -0,0 +1,28 @@
+const partConfig = {
+  parts: {
+    [
+      "TAIL",
+      "WINGS",
+      "BODY",
+      "EYES",
+      "GLASSES",
+      "EARS",
+      "NOSE",
+      "HORNS",
+      "HAT"
+    ]
+  },
+  count: {
+    TAIL : 1,
+    WINGS: 1,
+    BODY : 1,
+    EYES : 1,
+    GLASSES : 1,
+    EARS : 1,
+    NOSE : 1,
+    HORNS : 1,
+    HAT : 1
+  }
+};
+
+module.exports = partConfig;
